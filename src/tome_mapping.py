98947e0c7a29eb281a8f733a95adca015d8087e7
gemoyk <gemoyk@outlook.com>
2020-05-15 22:06:37 +0800

    create project

---
+++ src/tome_mapping.py
@@ -0,0 +1,14 @@
+"""
+@project: luminance
+
+@author: gemoy
+
+@file: tome_mapping
+
+Create on 2020/5/15 21:57
+
+Contact author by e-mail: gemo-yk@outlook.com
+"""
+import numpy as np
+import cv2
+
